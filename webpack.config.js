0f2c3e1b349e1a0f93177a41d66af80cac54889c
msoh <journy002@naver.com>
2022-01-14 01:20:25 +0900

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  // js 지정
   entry: "./js/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
     clean: true,
+  },
+
+  // html 지정
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+  ],
+
+  // localhost 지정
+  devServer: {
+    host: "localhost",
   },
 };
